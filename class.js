2c0d62c0624baf65ff8144de895a2ebd2bd1e236
Fuad Hasan <fuad.hasan.1997@gmail.com>
2020-09-24 04:50:07 +0600

    Arnold

---
+++ class.js
@@ -0,0 +1,13 @@
+class Student {
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "North South" 
+    }
+}
+
+const student1 = new Student(26, "NawaL");
+const student2 = new Student(9, "Fuad");
+const student3 = new Student(11, "Hasan");
+
+console.log(student3);
